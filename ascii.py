86003be34cea40dd38f1a44699ad909363067364
Jamal Elmir <81425259+blankprogram@users.noreply.github.com>
2024-07-05 04:23:49 +1000

    optimisations using np array's,preprocessing,bulk drawing

--- ascii.py
+++ ascii.py
@@ -1,12 +1,9 @@
 import argparse
 import itertools
-from random import randrange
-import sys
 import os
+import time
 from PIL import Image, ImageDraw, ImageFont, ImageSequence
 import numpy as np
-import time
-
 
 
 class TransparentAnimatedGifConverter:
@@ -17,30 +14,37 @@ class TransparentAnimatedGifConverter:
         self.alpha_threshold = alpha_threshold
 
     def _process_pixels(self):
-        self.transparent_pixels = {idx for idx, alpha in enumerate(self.img_rgba.getchannel('A').getdata()) if alpha <= self.alpha_threshold}
+        self.transparent_pixels = (
+            np.array(self.img_rgba.getchannel("A")) <= self.alpha_threshold
+        )
 
     def _get_palette(self):
         palette = self.img_p.getpalette()
-        self.used_palette_idxs = set(self.img_p_data)
-        self.parsed_palette = {idx: tuple(palette[idx * 3:idx * 3 + 3]) for idx in self.used_palette_idxs}
+        self.img_p_data = np.array(self.img_p)
+        self.used_palette_idxs = np.unique(self.img_p_data)
+        self.parsed_palette = {
+            idx: tuple(palette[idx * 3 : idx * 3 + 3]) for idx in self.used_palette_idxs
+        }
 
     def _get_similar_color_idx(self):
         old_color = self.parsed_palette[0]
-        distances = {idx: sum(abs(old_color[i] - color_item[i]) for i in range(3))
-                     for idx, color_item in self.parsed_palette.items() if idx != 0}
+        distances = {
+            idx: np.sum(np.abs(np.array(old_color) - np.array(color_item)))
+            for idx, color_item in self.parsed_palette.items()
+            if idx != 0
+        }
         return min(distances, key=distances.get)
 
     def _remap_palette_idx_zero(self):
-        free_slots = self._PALETTE_SLOTSET - self.used_palette_idxs
+        free_slots = self._PALETTE_SLOTSET - set(self.used_palette_idxs)
         new_idx = free_slots.pop() if free_slots else self._get_similar_color_idx()
-        self.used_palette_idxs.add(new_idx)
-        self.img_p_data = bytearray(new_idx if p == 0 else p for p in self.img_p_data)
+        self.img_p_data[self.img_p_data == 0] = new_idx
         self.parsed_palette[new_idx] = self.parsed_palette.pop(0)
 
     def _find_new_color(self):
         used_colors = set(self.parsed_palette.values())
         while True:
-            new_color = tuple(randrange(256) for _ in range(3))
+            new_color = tuple(np.random.randint(0, 256, 3))
             if new_color not in used_colors:
                 return new_color
 
@@ -51,24 +55,26 @@ class TransparentAnimatedGifConverter:
         self.parsed_palette[0] = self._find_new_color()
 
     def _adjust_pixels(self):
-        self.img_p_data = bytearray(self.img_p_data)
-        for idx in self.transparent_pixels:
-            self.img_p_data[idx] = 0
-        self.img_p.putdata(self.img_p_data)
+        self.img_p_data[self.transparent_pixels] = 0
+        self.img_p.putdata(self.img_p_data.flatten().tolist())
 
     def _adjust_palette(self):
-        palette = list(itertools.chain.from_iterable(self.parsed_palette.get(i, (0, 0, 0)) for i in range(256)))
+        palette = list(
+            itertools.chain.from_iterable(
+                self.parsed_palette.get(i, (0, 0, 0)) for i in range(256)
+            )
+        )
         self.img_p.putpalette(palette)
 
     def process(self) -> Image:
-        self.img_p = self.img_rgba.convert('P', palette=Image.ADAPTIVE, colors=256)
-        self.img_p_data = bytearray(self.img_p.tobytes())
+        self.img_p = self.img_rgba.convert("P", palette=Image.ADAPTIVE, colors=256)
+        self.img_p_data = np.array(self.img_p)
         self._process_pixels()
         self._process_palette()
         self._adjust_pixels()
         self._adjust_palette()
-        self.img_p.info['transparency'] = 0
-        self.img_p.info['background'] = 0
+        self.img_p.info["transparency"] = 0
+        self.img_p.info["background"] = 0
         return self.img_p
 
 
@@ -82,26 +88,36 @@ class AsciiArtConverter:
     def _image_to_ascii(self, image):
         aspect_ratio = image.height / image.width
         new_height = int(aspect_ratio * self._new_width * 0.55)
-        image = image.resize((self._new_width, new_height), Image.ANTIALIAS).convert('RGBA')
+        image = image.resize((self._new_width, new_height), Image.ANTIALIAS)
 
-        image_grey = np.asarray(image.convert('L'))
+        image_grey = np.asarray(image.convert("L"))
         image_color = np.asarray(image)
         alpha_channel = np.asarray(image.split()[-1])
 
         scale_factor = len(self._chars) / 256
+        scaled_chars = [self._chars[int(i * scale_factor)] for i in range(256)]
 
-        output_image = Image.new('RGBA', (self._new_width * 10, new_height * 20))
+        output_image = Image.new("RGBA", (self._new_width * 10, new_height * 20))
         draw = ImageDraw.Draw(output_image)
 
-        for y in range(new_height):
-            for x in range(self._new_width):
-                pixel = image_grey[y, x]
-                color = image_color[y, x]
-                ascii_char = self._chars[int(pixel * scale_factor)]
-                if alpha_channel[y, x] > 128:
-                    if self._fill != None:
-                        draw.rectangle([x * 10, y * 20, (x + 1) * 10, (y + 1) * 20], fill=self._fill)
-                    draw.text((x * 10, y * 20), ascii_char, fill=tuple(color[:3]), font=self._font)
+        valid_pixels = np.column_stack(np.where(alpha_channel > 128))
+        rectangles = []
+        texts = []
+
+        for y, x in valid_pixels:
+            pixel = image_grey[y, x]
+            color = tuple(image_color[y, x][:3])
+            ascii_char = scaled_chars[pixel]
+            x_pos, y_pos = x * 10, y * 20
+            if self._fill is not None:
+                rectangles.append([x_pos, y_pos, x_pos + 10, y_pos + 20])
+            texts.append((x_pos, y_pos, ascii_char, color))
+
+        for rect in rectangles:
+            draw.rectangle(rect, fill=self._fill)
+
+        for x_pos, y_pos, ascii_char, color in texts:
+            draw.text((x_pos, y_pos), ascii_char, fill=color, font=self._font)
 
         return output_image
 
@@ -111,56 +127,51 @@ class AsciiArtConverter:
         durations = []
 
         for frame in ImageSequence.Iterator(gif):
-            ascii_frame = self._image_to_ascii(frame)
-            frames.append(ascii_frame)
-            durations.append(frame.info.get('duration', 100))
-
-        processed_frames = []
-        for frame in frames:
-            converter = TransparentAnimatedGifConverter(frame)
+            ascii_frame = self._image_to_ascii(frame.convert("RGBA"))
+            converter = TransparentAnimatedGifConverter(ascii_frame)
             processed_frame = converter.process()
-            processed_frames.append(processed_frame)
+            frames.append(processed_frame)
+            durations.append(frame.info.get("duration", 100))
 
-        processed_frames[0].save(
+        frames[0].save(
             output_path,
             save_all=True,
-            append_images=processed_frames[1:],
+            append_images=frames[1:],
             loop=0,
             duration=durations,
-            disposal=2
+            disposal=2,
+            transparency=0,
         )
 
     def process_image(self, input_path, output_path):
-        if input_path.lower().endswith('.gif'):
+        if input_path.lower().endswith(".gif"):
             output_path = os.path.splitext(output_path)[0] + "_ascii.gif"
             self._process_gif(input_path, output_path)
             print(f"ASCII art GIF saved to {output_path}")
         else:
-            ascii_image = self._image_to_ascii(Image.open(input_path).convert('RGBA'))
+            ascii_image = self._image_to_ascii(Image.open(input_path).convert("RGBA"))
             output_path = os.path.splitext(output_path)[0] + "_ascii.png"
             ascii_image.save(output_path)
             print(f"ASCII art image saved to {output_path}")
 
     def process_directory(self, input_dir, output_dir):
         for filename in os.listdir(input_dir):
-            if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
+            if filename.lower().endswith((".png", ".jpg", ".jpeg", ".bmp", ".gif")):
                 input_path = os.path.join(input_dir, filename)
                 output_path = os.path.join(output_dir, os.path.splitext(filename)[0])
                 self.process_image(input_path, output_path)
 
 
 def main():
-    if len(sys.argv) < 3:
-        print("Usage: ascii.py <input file/directory> <output file/directory> [new_width] [chars] [font_path] [font_colour]")
-        return
-
-    parser = argparse.ArgumentParser(description="converts images/gifs to their ascii equivelant")
-    parser.add_argument("input_path",type=str)
-    parser.add_argument("output_path",type=str)
-    parser.add_argument("--width",type=int,default=100)
-    parser.add_argument("--chars",type=str,default=".:-=+*#%@")
-    parser.add_argument("--font",type=str,default=None)
-    parser.add_argument("--fill",type=str,default=None)
+    parser = argparse.ArgumentParser(
+        description="converts images/gifs to their ascii equivalent"
+    )
+    parser.add_argument("input_path", type=str)
+    parser.add_argument("output_path", type=str)
+    parser.add_argument("--width", type=int, default=100)
+    parser.add_argument("--chars", type=str, default=".:-=+*#%@")
+    parser.add_argument("--font", type=str, default=None)
+    parser.add_argument("--fill", type=str, default=None)
 
     args = parser.parse_args()
     input_path = args.input_path
@@ -168,11 +179,11 @@ def main():
     new_width = args.width
     chars = args.chars
     fill = args.fill
-    font = ImageFont.truetype(args.font,10) if args.font else ImageFont.load_default()
-    
+    font = ImageFont.truetype(args.font, 10) if args.font else ImageFont.load_default()
+
     converter = AsciiArtConverter(new_width, chars, font, fill)
     start_time = time.time()
-    
+
     if os.path.isdir(input_path):
         if not os.path.exists(output_path):
             os.makedirs(output_path)
@@ -189,5 +200,5 @@ def main():
     print(f"Elapsed time: {elapsed_time:.2f} seconds")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
